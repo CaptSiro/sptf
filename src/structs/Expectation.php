1d4dae2e423cdad429d2876e4b4e0f168bd403b9
CaptSiro <honzahorak433@icloud.com>
2023-09-07 16:47:52 +0200

    Fixed incorrect design decision

--- src/structs/Expectation.php
+++ src/structs/Expectation.php
@@ -1,8 +1,8 @@
 <?php
 
 
-namespace sptf\structs;
 
+namespace sptf\structs;
 
 use Closure;
 use sptf\interfaces\Assertion;
@@ -11,15 +11,17 @@ use sptf\interfaces\Expect;
 require_once __DIR__ . "/../interfaces/Assertion.php";
 require_once __DIR__ . "/../interfaces/Expect.php";
 
+
+
 class Expectation implements Assertion, Expect {
-    private mixed $actual;
+    private mixed $expected;
     private Closure $compare;
     private readonly int $line;
 
 
 
     function __construct(
-        private readonly mixed $expected,
+        private readonly mixed $actual,
         array $trace
     ) {
         $this->line = $trace[0]['line'];
@@ -28,7 +30,7 @@ class Expectation implements Assertion, Expect {
 
 
     function toBe(mixed $value): self {
-        $this->actual = $value;
+        $this->expected = $value;
         return $this;
     }
 
